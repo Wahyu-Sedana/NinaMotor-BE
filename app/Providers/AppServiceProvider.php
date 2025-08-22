7ef5a0f48121ec192fa8781112abe19f957e56ce
Wahyu-Sedana <wsstudiocode@gmail.com>
2025-08-22 18:27:32 +0800

    handle ajx https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         // Share admin user with all views
         View::composer('*', function ($view) {
             $adminUser = null;
